6c0588895081d4c13ee89f1138c622d0b838b481
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 04:17:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,11 @@ def prime?(integer)
   if integer >= 2 
       (2..integer - 1).all? do |x| 
         integer % x != 0
-      return true  
       end
   else
     return false
   end 
-end 
+
 
 =begin
 def prime?(number)
