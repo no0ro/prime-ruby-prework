a2f831f0e32964d22ce0b206845a18a7eb1c4690
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 03:46:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
 def prime?(integer)
   range = 2..integer
+  if integer ==
   for i in range
   
   end
 end 
+  for (int i = 2; i < n; i++) 
+        if (n % i == 0) 
+            return false; 
+  
+    return true; 
