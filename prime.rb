56764d2f56e7a3e715f2a87765957ea297fa89f0
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 04:21:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,14 +7,3 @@ def prime?(integer)
     return false
   end 
 end 
-
-=begin
-def prime?(number)
-  if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-    end
-  else
-    return false
-  end
-=end 
