0145176353b32efa5182931c7b06d696a4c95304
Noelle Rolwes <nrolwes@gmail.com>
2018-09-11 03:56:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,18 @@
 def prime?(integer)
   range = 2..integer
-  if integer <= 1 
-    false
+  if integer <= 2 
+    (2..integer - 1).all? do 
   for i in range
     if range%i == 0
     false
   end
 end 
+
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
